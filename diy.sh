c4d601b6454072df5e27440a5c536a86be1ed5bd
jh2921291 <42035572+jh2921291@users.noreply.github.com>
2020-04-18 11:06:23 +0800

    更新配置

--- diy.sh
+++ diy.sh
@@ -9,6 +9,12 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 
+#!/bin/bash 添加ssrp
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+
+
 # git clone https://github.com/P3TERX/xxx package/xxx
 
 # ./scripts/feeds update -a
