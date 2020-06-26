b36377184d248775dd13283d802799420b8a246b
jh2921291 <42035572+jh2921291@users.noreply.github.com>
2020-06-27 07:00:03 +0800

    updatelua

---
+++ diy-part1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#!/bin/bash 添加ssrp
+git clone https://github.com/fw876/helloworld.git package/ssrp
+
+#!/bin/bash openclash
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+
+
+./scripts/feeds update -a
+./scripts/feeds install -a
